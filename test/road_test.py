2a88c2a286e49aa363435801d22fe9102c5c9343
Fatima Davelouis Gallardo <daveloui@mbowling-MacBook-Pro.local>
2018-06-05 12:54:21 -0600

    Test to see if the transition states have probability = 1.0.

--- test/road_test.py
+++ test/road_test.py
@@ -16,15 +16,27 @@ def test_transition_probs_without_obstacles_are_always_1():
     for a in ACTIONS:
         for next_state, prob, reward in road_test.successors(a):
             assert prob == 1.0
+# -----------------------------------------------------------------------------------------------------
 
-def test_transition_probs_with_one_obstacle_are_1(): #here we test what happens when the car hits/doesn't hit the obstacle
+# The following tests whether we obtain a probability == 1.0 if either a bump or a pedestrian collides or does not collide with the car.
+import pytest
+@pytest.mark.parametrize("obst", [Bump(2, 2)], Pedestrian(2, 2)])
+@pytest.mark.parametrize("car_inst", [Car(3, 2, 1)], Car(3, 1, 1)]) # first will have collision, second will have no collision
+
+
+def test_transition_probs_with_one_obstacle_are_1(obst, car_inst):
     num_rows = 2
-#    obst = Bump(2, 2)
-    obst = Pedestrian(2, 2)
     obstacles = [obst]
     speed_limit = 1
-    car_inst = Car(3, 2, 1) # in this case, the car hits the obstacle
+
     road_test = Road(num_rows, car_inst, obstacles, speed_limit)
     for a in ACTIONS:
+#        print("action = " + str(a))
         for next_state, prob, reward in road_test.successors(a):
+            if obst.next(car_inst).has_collided(car_inst):
+#                print("has collided")
+#                print(str(prob))
+            else:
+                print("no collision")
+#                print(str(prob))
             assert prob == 1.0
