0b44280792c222a1ccbbc67e6edbe280b438fa21
Dustin Morrill <dmorrill10@gmail.com>
2018-06-06 18:27:28 -0600

    Revert "Test to see if the transition states have probability = 1.0 with one obstacle."

--- test/road_test.py
+++ test/road_test.py
@@ -1,4 +1,3 @@
-import pytest
 from driving_gridworld.road import Road
 from driving_gridworld.obstacles import Obstacle
 from driving_gridworld.obstacles import Bump
@@ -13,17 +12,18 @@ def test_transition_probs_without_obstacles_are_always_1():
     speed_limit = 1
     car_inst = Car(0, 0, 1)
     road_test = Road(num_rows, car_inst, obstacles, speed_limit)
+
     for a in ACTIONS:
         for next_state, prob, reward in road_test.successors(a):
             assert prob == 1.0
 
-
-@pytest.mark.parametrize("obst", [Bump(2, 2), Pedestrian(2, 2)])
-@pytest.mark.parametrize("car_inst", [Car(3, 2, 1)])
-def test_transition_probs_with_one_obstacle_are_1(obst, car_inst):
+def test_transition_probs_with_one_obstacle_are_1(): #here we test what happens when the car hits/doesn't hit the obstacle
     num_rows = 2
+#    obst = Bump(2, 2)
+    obst = Pedestrian(2, 2)
     obstacles = [obst]
     speed_limit = 1
+    car_inst = Car(3, 2, 1) # in this case, the car hits the obstacle
     road_test = Road(num_rows, car_inst, obstacles, speed_limit)
     for a in ACTIONS:
         for next_state, prob, reward in road_test.successors(a):
