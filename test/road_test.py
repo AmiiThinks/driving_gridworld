219d0c0d0f34afc0fb04b2204e19cfd30c2ea892
Dustin Morrill <dmorrill10@gmail.com>
2018-06-08 10:05:37 -0600

    Fix transition probability bug. (#5)

--- test/road_test.py
+++ test/road_test.py
@@ -1,9 +1,9 @@
 from driving_gridworld.road import Road
-from driving_gridworld.obstacles import Obstacle
 from driving_gridworld.obstacles import Bump
 from driving_gridworld.obstacles import Pedestrian
 from driving_gridworld.car import Car
 from driving_gridworld.actions import ACTIONS
+import pytest
 
 
 def test_transition_probs_without_obstacles_are_always_1():
@@ -17,14 +17,30 @@ def test_transition_probs_without_obstacles_are_always_1():
         for next_state, prob, reward in road_test.successors(a):
             assert prob == 1.0
 
-def test_transition_probs_with_one_obstacle_are_1(): #here we test what happens when the car hits/doesn't hit the obstacle
+
+@pytest.mark.parametrize("obst", [Bump(0, 0), Pedestrian(0, 0)])
+def test_no_obstacles_revealed_is_the_only_valid_set_of_revealed_obstacles_when_all_obstacles_already_on_road(obst):
     num_rows = 2
-#    obst = Bump(2, 2)
-    obst = Pedestrian(2, 2)
-    obstacles = [obst]
     speed_limit = 1
-    car_inst = Car(3, 2, 1) # in this case, the car hits the obstacle
-    road_test = Road(num_rows, car_inst, obstacles, speed_limit)
-    for a in ACTIONS:
-        for next_state, prob, reward in road_test.successors(a):
-            assert prob == 1.0
+
+    road_test = Road(num_rows, Car(1, 1, 1), [obst], speed_limit)
+    patient = [
+        (positions, reveal_indices)
+        for positions, reveal_indices in
+        road_test.every_combination_of_revealed_obstacles()
+    ]
+    assert patient == [(tuple(), set())]
+
+
+@pytest.mark.parametrize("obst", [Bump(0, 0), Pedestrian(0, 0)])
+@pytest.mark.parametrize("action", ACTIONS)
+def test_transition_probs_with_one_obstacle_are_1(obst, action):
+    num_rows = 2
+    speed_limit = 1
+
+    road_test = Road(num_rows, Car(1, 1, 1), [obst], speed_limit)
+    probs = [
+        prob
+        for next_state, prob, reward in road_test.successors(action)
+    ]
+    assert probs == [1.0]
