8e30e5cdc9b446e86980cc6277e62c27034a81ce
Dustin Morrill <dmorrill10@gmail.com>
2018-06-12 18:25:24 -0600

    Improve style.

--- test/obstacles_test.py
+++ test/obstacles_test.py
@@ -7,14 +7,16 @@ def test_creation():
     assert patient.row == 2
     assert patient.col == 2
 
+
 def test_no_collision_mismatched_columns():
     patient = Obstacle(2, 2)
     car = Car(3, 1, 1)
     assert not patient.next(car).has_collided(car)
+
 
 def test_collision_matched_columns():
     patient = Obstacle(2, 2)
     car = Car(3, 2, 1)
     next_patient = patient.next(car)
     assert next_patient.row == 3
-    assert next_patient.has_collided(car)  
+    assert next_patient.has_collided(car)
