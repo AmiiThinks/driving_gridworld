c171280dfcf0827dcf8e71ca44ba989c17b08bf3
Dustin Morrill <dmorrill10@gmail.com>
2018-06-27 16:47:28 -0600

    Simplify `hand_coded_data_gathering_policy`.

--- driving_gridworld/policies.py
+++ driving_gridworld/policies.py
@@ -44,26 +44,12 @@ def hand_coded_obstacle_score(obst, road):
 
 def hand_coded_data_gathering_policy(road):
     scores = hand_coded_score_for_columns_adjacent_to_car(road)
-    max_idx = scores.index(max(scores))
-    if max_idx == 0 and scores[0] == scores[1]:
-        max_idx = 1
-
-    if max_idx == 0:
-        return LEFT
-
-    elif max_idx == 2:
-        return RIGHT
-
-    elif max_idx == 1 and scores[1] < 0:
-        if road._car.speed == 1:
-            return NO_OP
-        else:
-            return DOWN
-
-    elif max_idx == 1 and scores[1] >= 0:
-        if road._car.speed >= road._headlight_range:
-            return DOWN
-        elif road._car.speed < road._headlight_range - 1:
-            return UP
-        else:
-            return NO_OP
+    max_idx = np.argmax(scores)
+    if scores[max_idx] == scores[1]: max_idx = 1
+    if max_idx == 0: return LEFT
+    elif max_idx == 2: return RIGHT
+    else:
+        if scores[1] < 0 and road._car.speed > 1: return DOWN
+        elif road._car.speed >= road._headlight_range: return DOWN
+        elif road._car.speed < road._headlight_range - 1: return UP
+    return NO_OP
