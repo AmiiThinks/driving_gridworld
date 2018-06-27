32ee0f516bec8190320bf05ee4bb81b708112a25
Dustin Morrill <dmorrill10@gmail.com>
2018-06-27 17:30:49 -0600

    Simplify function.

--- driving_gridworld/policies.py
+++ driving_gridworld/policies.py
@@ -5,31 +5,11 @@ from driving_gridworld.actions import UP, DOWN, RIGHT, LEFT, NO_OP
 
 
 def hand_coded_score_for_columns_adjacent_to_car(road):
-    adj_cols_idx = list(
-        range(max(0, road._car.col - 1), min(road._car.col + 2, 4)))
-    scores = [0] * len(adj_cols_idx)
-
-    if road._car.col == 0:
-        scores.insert(0, -np.inf)
-        scores[1] = -2
-
-    elif road._car.col == 1:
-        scores[0] = -2
-
-    elif road._car.col == 2:
-        scores[2] = -2
-
-    else:
-        scores.append(-np.inf)
-        scores[1] = -2
-
+    scores = [-np.inf, -2, 0, 0, -2, -np.inf]
     for obst in road._obstacles:
         if road.obstacle_is_visible(obst):
-            delta = abs(obst.col - road._car.col)
-            if delta < 2:
-                i = obst.col - road._car.col + 1
-                scores[i] += hand_coded_obstacle_score(obst, road)
-    return scores
+            scores[obst.col + 1] += hand_coded_obstacle_score(obst, road)
+    return scores[road._car.col:road._car.col + 3]
 
 
 def hand_coded_obstacle_score(obst, road):
